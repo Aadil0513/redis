25042b13036f17ee43792b9690d44a2907f70459
M Aadil Rajputecho <maadilrajput05@gmail.com>
2026-06-30 16:01:35 +0500

    redis practice

---
+++ string.js
@@ -0,0 +1,15 @@
+
+import  client  from "./client.js";
+
+
+async function init(){
+
+           await client.set('msg:6' , "Hey from node js")
+    const result = await client.get('msg:6')
+
+    console.log("result->", result)
+
+
+}
+
+init()
