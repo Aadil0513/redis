76973cf9afff8a8f73d1b43640063cc3abd97945
M Aadil Rajputecho <maadilrajput05@gmail.com>
2026-07-01 23:01:23 +0500

    2nd codewars problem

--- codewars.js
+++ codewars.js
@@ -1,4 +1,22 @@
-function sum(numbers) {
-  return numbers.reduce((a, b) => a + b, 0);
-}
 
+// solved
+// function sum(numbers) {
+//   return numbers.reduce((a, b) => a + b, 0);
+// }
+
+
+
+// not solve yet 
+function digitize(n) {
+  //code here
+  let arry =[]
+  
+  let num = n.toString().split("")
+   
+   for(let i = num.length ; i>=0 ; i-- ){
+     
+     arry[i] += num[i]
+   }
+  
+  return +arry
+}
