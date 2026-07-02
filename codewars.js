09beae94cd5ea2e54214d9874ce4ca8fe19ffabf
M Aadil Rajputecho <maadilrajput05@gmail.com>
2026-07-02 22:49:29 +0500

    new codewars probelms solved

--- codewars.js
+++ codewars.js
@@ -20,3 +20,33 @@ function digitize(n) {
   
   return +arry
 }
+
+
+
+
+ 
+function findOdd(A) {
+  // .reduce() array ke har element par loop chalayega aur XOR apply karega
+  return A.reduce((accumulator, current) => accumulator ^ current, 0);
+}
+
+// Test Cases
+console.log(findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5])); // Output: 5
+console.log(findOdd([1, 1, 2])); // Output: 2
+
+
+function findOddAlternative(A) {
+  const count = {};
+  
+  // Har number ka count store karna
+  for (let num of A) {
+    count[num] = (count[num] || 0) + 1;
+  }
+  
+  // Woh number dhundna jiska count odd hai
+  for (let num in count) {
+    if (count[num] % 2 !== 0) {
+      return Number(num); // Object keys hamesha string hoti hain, isliye number mein convert kiya
+    }
+  }
+}
